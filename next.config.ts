4ea2900f0b5b130d8eb8807827ae21cef628a75f
doctorboyz <s.adisorn@gmail.com>
2026-02-12 17:28:02 +0700

    Remove standalone output for Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: false,
-  output: "standalone",
 };
 
 export default nextConfig;
